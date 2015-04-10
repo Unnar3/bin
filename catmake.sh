45ba87a0362692ae3275d34b78882703cb7fb4db
Unnar3 <unnar3@gmail.com>
2015-04-10 15:24:31 +0200

    added params to catmake

--- catmake.sh
+++ catmake.sh
@@ -1,10 +1,48 @@
 #!/bin/bash --
 yellow='\033[1;33m'
 NC='\033[0m' # No Color
-if [ -z "$1" ]; then
-    echo -e "${yellow}Running catkin_make${NC}"
-    (cd ~/catkin_ws && catkin_make)
+
+PKG1=""
+PKG2=""
+ERR=""
+PA=""
+
+
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -pkg|--pkg)
+    PKG2="$2"
+    PKG1="--pkg"
+    shift
+    ;;
+    -err|--error)
+    ERR="e"
+    if [[ -z $2 ]]; then
+    	PA=$PWD
+	else
+		PA="$2"
+	fi
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift
+done
+
+if [[ -z "$PKG1" ]]; then
+	echo -e "${yellow}Running catkin_make${NC}"
 else
-	echo -e "${yellow}Running catkin_make on package $1 ${NC}"
-	(cd ~/catkin_ws && catkin_make --pkg $1)
+	echo -e "${yellow}Running catkin_make on package $PKG2 ${NC}"
+fi
+
+if [[ -z "$ERR" ]]; then
+	(cd ~/catkin_ws && catkin_make ${PKG1} ${PKG2})
+else
+	echo -e "${yellow}ERROR log in folder: ${PA} ${NC}"
+	(cd ~/catkin_ws && catkin_make ${PKG1} ${PKG2} 2>&1 1>${PA}/stdout.log | tee ${PA}/stderr.log)
 fi
---
+++ setupROS.sh
@@ -0,0 +1,5 @@
+#!/bin/bash --
+yellow='\033[1;33m'
+NC='\033[0m' # No Color
+echo -e "${yellow}source setup.bash for ROS${NC}"
+source /home/unnar/catkin_ws/devel/setup.bash
